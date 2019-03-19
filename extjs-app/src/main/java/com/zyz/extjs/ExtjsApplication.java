2e1eb70333eac9dc0d5cdd728227bea41d0c2d53
zyz777 <260174798@qq.com>
2019-03-19 22:10:21 +0800

    提交

---
+++ extjs-app/src/main/java/com/zyz/extjs/ExtjsApplication.java
@@ -0,0 +1,13 @@
+package com.zyz.extjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtjsApplication.class, args);
+    }
+
+}
---
+++ extjs-app/src/main/java/com/zyz/extjs/controller/Extjs07.java
@@ -0,0 +1,18 @@
+package com.zyz.extjs.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 权威指南第7章
+ */
+@RestController
+@RequestMapping("/extjs_07")
+public class Extjs07 {
+
+    @RequestMapping("/")
+    public void test1() {
+
+    }
+
+}
