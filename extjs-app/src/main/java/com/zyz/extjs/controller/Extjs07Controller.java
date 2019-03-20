bcaaffb40a0fc656c8490d63082992a46def9839
zyz777 <260174798@qq.com>
2019-03-20 15:45:55 +0800

    提交

--- extjs-app/src/main/java/com/zyz/extjs/controller/Extjs07Controller.java
+++ extjs-app/src/main/java/com/zyz/extjs/controller/Extjs07Controller.java
@@ -2,8 +2,16 @@ package com.zyz.extjs.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zyz.extjs.entity.Products;
 import com.zyz.extjs.entity.Test;
 import org.springframework.web.bind.annotation.*;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.stream.DoubleStream;
 
 /**
  * 权威指南第7章
@@ -14,6 +22,10 @@ import org.springframework.web.bind.annotation.*;
 public class Extjs07Controller {
 
     ObjectMapper mapper = new ObjectMapper();
+
+    //*********************************
+    //  03-proxy -> proxy.html
+    //*********************************
 
     @RequestMapping(value = "test/{id}", method = RequestMethod.GET)
     public Test test1(@PathVariable("id") int id) {
@@ -31,6 +43,96 @@ public class Extjs07Controller {
     @RequestMapping(value = "test/{id}", method = RequestMethod.DELETE)
     public Test test4(@PathVariable("id") int id) {
         return new Test(4, "删除");
+    }
+
+
+
+    //************************************
+    //  06-store  ->  store.html
+    //************************************
+    private static List<Products> list = new ArrayList<>();
+    static {
+        Random random = new Random();
+
+        Products products = null;
+        for (int i = 1; i <= 1345; i++) {
+            products = new Products();
+            list.add(products);
+
+            products.setId(i);
+            products.setProductName("产品名称"+i);
+
+            int num = random.nextInt(10);//0-9的随机数
+            products.setSupplierId(num);
+
+            num = random.nextInt(10);//0-9的随机数
+            products.setCategoryId(num);
+
+            products.setQuantityPerUnit(UUID.randomUUID().toString());
+            products.setUnitPrice((float)(Math.random()*100));
+
+            num = random.nextInt(200);
+            products.setUnitsInStock(num);
+
+            num = random.nextInt(200);
+            products.setUnitsOnOrder(num);
+
+            num = random.nextInt(6);
+            products.setReorderLevel(num);
+
+            num = random.nextInt(11);
+            products.setDiscontinued(num>5);
+        }
+    }
+
+    class Response {
+        private boolean success = true;
+        private int total;
+        private Object data;
+
+        public Response(int total, Object data) {
+            this.total = total;
+            this.data = data;
+        }
+
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public void setSuccess(boolean success) {
+            this.success = success;
+        }
+
+        public int getTotal() {
+            return total;
+        }
+
+        public void setTotal(int total) {
+            this.total = total;
+        }
+
+        public Object getData() {
+            return data;
+        }
+
+        public void setData(Object data) {
+            this.data = data;
+        }
+    }
+
+    //page=1&start=0&limit=20
+    @RequestMapping("/products")
+    public Response list(int page, int start, int limit) {
+        System.out.println(MessageFormat.format("page={0}, start={1}, limit={2}", page, start, limit));
+
+        int startIndex = (page-1)*limit;
+        int endIndex = page * limit;
+        int size = list.size();
+        List<Products> rsList = new ArrayList<>(limit);
+        for (int i = startIndex; i < endIndex && i<=size; i++) {
+            rsList.add(list.get(i));
+        }
+        return new Response(size, rsList);
     }
 
 }
---
+++ extjs-app/src/main/java/com/zyz/extjs/entity/Products.java
@@ -0,0 +1,98 @@
+package com.zyz.extjs.entity;
+
+public class Products {
+
+    private Integer id;
+    private String productName;
+    private Integer supplierId;
+    private Integer categoryId;
+    private String quantityPerUnit;
+    private Float unitPrice;
+    private Integer unitsInStock;
+    private Integer unitsOnOrder;
+    private Integer reorderLevel;
+    private boolean discontinued;
+
+    public Products() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(Integer supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getQuantityPerUnit() {
+        return quantityPerUnit;
+    }
+
+    public void setQuantityPerUnit(String quantityPerUnit) {
+        this.quantityPerUnit = quantityPerUnit;
+    }
+
+    public Float getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Float unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Integer getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(Integer unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public Integer getUnitsOnOrder() {
+        return unitsOnOrder;
+    }
+
+    public void setUnitsOnOrder(Integer unitsOnOrder) {
+        this.unitsOnOrder = unitsOnOrder;
+    }
+
+    public Integer getReorderLevel() {
+        return reorderLevel;
+    }
+
+    public void setReorderLevel(Integer reorderLevel) {
+        this.reorderLevel = reorderLevel;
+    }
+
+    public boolean isDiscontinued() {
+        return discontinued;
+    }
+
+    public void setDiscontinued(boolean discontinued) {
+        this.discontinued = discontinued;
+    }
+}
